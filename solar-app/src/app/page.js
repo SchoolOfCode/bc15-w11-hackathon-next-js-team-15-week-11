ec3ef4d3d11d5f7794177b74f516bb69a5f2eb37
DougKF <douglaskforbes@gmail.com>
2023-11-17 12:03:26 +0000

    Removed Pluto and the Sun

--- solar-app/src/app/page.js
+++ solar-app/src/app/page.js
@@ -5,14 +5,12 @@ import { fetchData } from "./components/planetDataCache";
 const hotspots = [
   { id: 1, x: 49, y: 78, label: "Venus", sizePercentage: 9.5, link: "/venus" },
   { id: 2, x: 49, y: 44.7, label: "Jupiter", sizePercentage: 19, link: "/jupiter" },
-  { id: 3, x: 5, y: 86.5, label: "Sun", sizePercentage: 2, link: "/sun" },
   { id: 4, x: 49.1, y: 87.1, label: "Mercury", sizePercentage: 7, link: "/mercury" },
   { id: 5, x: 48.9, y: 67.55, label: "Earth", sizePercentage: 9.7, link: "/earth" },
   { id: 6, x: 49, y: 56.7, label: "Mars", sizePercentage: 6.5, link: "/mars" },
   { id: 7, x: 49.6, y: 26.8, label: "Saturn", sizePercentage: 10.5, link: "/saturn" },
   { id: 8, x: 49.1, y: 14.18, label: "Uranus", sizePercentage: 6.8, link: "/uranus" },
   { id: 9, x: 48.9, y: 4.45, label: "Neptune", sizePercentage: 8, link: "/neptune" },
-  { id: 10, x: 92.4, y: 18.7, label: "Pluto", sizePercentage: 2, link: "/pluto" },
 ];
 
 export default async function Home() {
