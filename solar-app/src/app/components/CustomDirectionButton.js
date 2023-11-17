bfabca0daf1998f983a3a342f34535d420f158c6
Haya Minhas <faithwaheed@icloud.com>
2023-11-17 12:57:59 +0000

    custom button adjust, need to add picture into public folder

---
+++ solar-app/src/app/components/CustomDirectionButton.js
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Link from "next/link";
+
+export default function CustomDirectionButton({ url, left, right }) {
+    const direction = () => {
+      if (left) {
+        return (
+          <>
+            <Link href={url}>
+              <Image src="/left-polygon.png" width={50} height={50} />
+            </Link>
+          </>
+        );
+      } else if (right) {
+        return (
+          <Link href={url}>
+            <Image src="/right-polygon.png" width={50} height={50} />
+          </Link>
+        );
+      }
+      
+    };
--- solar-app/src/app/earth/page.jsx
+++ solar-app/src/app/earth/page.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import useMoons from "../components/useMoons";
+import CustomDirectionButton from "../components/CustomDirectionButton";
+
 
 const LaTerre = async () => {
   const data = await useMoons("La Terre"); // note must be the French name, as this matches the moon names
 
   return (
-    <div>
-      <h1 className="planet-name">{data.planet.englishName}</h1>
-      <h2>Moons</h2>
-      <div className="moonsContainerCol">{data.moons}</div>
-    </div>
+    <>
+      <div>
+        <h1 className="planet-name">{data.planet.englishName}</h1>
+        <h2>Moons</h2>
+        <div className="moonsContainerCol">{data.moons}</div>
+      </div>
+
+      <div>
+        <CustomDirectionButton url={"/venus"} left />
+        <br />
+        <CustomDirectionButton url={"/mars"} right />
+      </div>
+    </>
   );
 };
 
