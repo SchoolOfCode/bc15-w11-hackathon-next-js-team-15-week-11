179b38982534e2e3cb9c84bdc8a833acb62e5e36
James Robertson <jaarobertson92@gmail.com>
2023-11-17 14:07:50 +0000

    planet component

--- solar-app/src/app/components/Planets.jsx
+++ solar-app/src/app/components/Planets.jsx
@@ -2,12 +2,26 @@ import Link from "next/link";
 import PlanetCard from "./PlanetCard";
 import { planetData } from "./db";
 
-const Planet = ({data}) => {
+function Planet({data}) {
     return (
         <div>
-
-        </div>
+            <div>
+            </div>
+            {data.map(({id, name, distance, radius, temperature, day, year, moons, image, description }) => (
+                <PlanetCard
+                    name={name}
+                    distance={distance}
+                    radius={radius}
+                    temperature={temperature}
+                    day={day}
+                    year={year}
+                    moons={moons}
+                    image={image}
+                    description={description}
+                ></PlanetCard>
+            ))}
+            </div>
     )
-}
+            }
 
-
+    export default Planet
